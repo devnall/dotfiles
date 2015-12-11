2e06f84f90c00656a81a115380e116e4de3348a6
Drew Nall <devnall@gmail.com>
2015-12-11 11:53:02 -0500

    Add better notes to the tmux_clipboard script

--- bin/tmux_clipboard.sh
+++ bin/tmux_clipboard.sh
@@ -2,6 +2,7 @@
 #
 # This should help get tmux buffer copy/paste working better on OSX
 # See https://robots.thoughtbot.com/how-to-copy-and-paste-with-tmux-on-mac-os-x for more info
+# Basically, it should look to see if there are any buffers in tmux and, if there are, copy them to system clipboard and delete the buffer
 
 while true; do
   if test -n "`tmux showb 2> /dev/null`"; then
