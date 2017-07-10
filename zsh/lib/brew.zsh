2217d84c341ee369d26eba94f95fe520856b649c
Drew Nall <devnall@gmail.com>
2017-07-10 11:08:36 -0400

    Fix sourcing zsh/lib/* stuff; break brew upd/upg into functions

---
+++ zsh/lib/brew.zsh
@@ -0,0 +1,22 @@
+function brewupd() {
+  echo "---> Updating brews"
+  brew update
+  echo "---> The following packages can be upgraded:"
+  brew outdated
+}
+
+function brewupg() {
+  echo "---> Upgrading installed brews"
+  brew upgrade
+  echo "---> Cleaning up brews"
+  brew cleanup
+}
+
+function bubu() {
+  brewupd
+  brewupg
+  echo "---> Running brew doctor"
+  brew doctor
+  echo "---> Pruning old brew symlinks"
+  brew prune
+}
