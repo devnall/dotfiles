8b7ed4228e1810ff25fc4328dc96d3588d0454b8
Drew Nall <devnall@gmail.com>
2018-03-30 14:15:25 -0400

    add kubectl completions

--- zsh/lib/completions.zsh
+++ zsh/lib/completions.zsh
@@ -3,3 +3,9 @@ if [ -f /Users/dnall/homebrew/bin/aws_zsh_completer.sh ]
 then
   source /Users/dnall/homebrew/bin/aws_zsh_completer.sh
 fi
+
+# kubectl completions
+if [ -f /Users/dnall/homebrew/bin/kubectl ]
+then
+  source <(kubectl completion zsh)
+fi
