e5bac250544853678bb255963f604401ef16c263
Drew Nall <devnall@gmail.com>
2018-10-23 00:17:08 -0400

    Cleanup alias.zsh

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -8,9 +8,9 @@ alias _='sudo'
 
 if [[ `uname` == 'Darwin' ]]; then # MacOS
   ls_colorflag="-G"
-  #alias lsal='CLICOLOR_FORCE=1 ls -lahF ${ls_colorflag} | less -R'
-  alias lsal='CLICOLOR_FORCE=1 ls -lahF ${ls_colorflag} | bat'
-  if [[ -f ${brew_path}/bin/colorls ]]; then
+  alias lsal='CLICOLOR_FORCE=1 ls -lahF ${ls_colorflag} | less -R'
+  # TODO: modularize this path
+  if [[ -f "$HOME/homebrew/bin/colorls" ]]; then
     alias l='colorls -lA --sd --gs'
     alias ll='colorls -l --sd --gs'
     alias la='colorls -A --sd --gs'
@@ -30,9 +30,6 @@ else # Linux
   alias lsal='ls -lahF ${ls_colorflag} | less -R'
 fi
 
-# Homebrew
-alias brews='brew list -1|grep $1'
-
 # Other
 alias rm='rm -i'
 alias du='du -h'
@@ -43,47 +40,42 @@ alias po="popd"
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
+alias .....='cd ../../../..'
 alias ssr="ssh -l root"
 if hash fd 2>/dev/null; then
   alias find='echo "No! Use fd instead! So. Much. Faster."'
 fi
 
-# If bat is installed, use it instead of cat/less
-#if [[ -f ${brew_path}/bin/bat ]]; then
-#  alias cat="bat"
-#  alias less="bat"
-#fi
-
 # If prettyping is installed, use it instead of ping
-if [[ -f ${brew_path}/bin/prettyping ]]; then
+if [[ -f "$HOME/homebrew/bin/prettyping" ]]; then
   alias ping="prettyping --nolegend"
 fi
 
 # If htop is installed, use it instead of top
-if [[ -f ${brew_path}/bin/htop ]]; then
+if [[ -f "$HOME/homebrew/bin/htop" ]]; then
   alias top="htop"
 fi
 
 # If GNU date is installed, use it instead of old date shipped w/ MacOS
-if [ -f ${brew_path}/bin/gdate ]; then
+if [ -f "$HOME"/homebrew/bin/gdate ]; then
   alias date="gdate"
 fi
 
 # If ncdu is installed, use it instead of du
-if [[ -f ${brew_path}/bin/ncdu ]]; then
-  du="ncdu --color dark -rr -x --exclude .git --exclude node_modules"
+if [[ -f "$HOME/homebrew/bin/ncdu" ]]; then
+  alias du="ncdu --color dark -rr -x --exclude .git --exclude node_modules"
 fi
 
 # No `free` command on OSX, here's a hacky substitute
 if [[ `uname` == 'Darwin' ]]; then
-  alias free="top -l 1 -s 0 | grep PhysMem"
+  alias free="/usr/bin/top -l 1 -s 0 | grep PhysMem"
 fi
 
 # Get IP Addresses
 # TODO: Turn this into a function that can handle different OSes
 alias ip='echo "External IP:   " `dig +short myip.opendns.com @resolver1.opendns.com` && echo "Ethernet (en3):" `ipconfig getifaddr en3` && echo "Wireless (en0):" `ipconfig getifaddr en0`'
-alias localip='echo "Ethernet (en0):" `ipconfig getifaddr en0` && echo "Wireless (en1):" `ipconfig getifaddr en1`'
-alias ips='ifconfig -a | perl -nle"/(\d+\.\d+\.\d+\.\d+)/ && print $1"'
+#alias localip='echo "Ethernet (en0):" `ipconfig getifaddr en0` && echo "Wireless (en1):" `ipconfig getifaddr en1`'
+#alias ips='ifconfig -a | perl -nle"/(\d+\.\d+\.\d+\.\d+)/ && print $1"'
 
 # Recursively delete `.DS_Store` files
 alias dscleanup="find . -name '*.DS_Store' -type f -ls -delete"
